0da1f239c139db1c2a4bb16d32e31e25e8788047
felixskarlund <fesk16ab@student.cbs.dk>
2017-12-20 22:55:42 +0100

    Commenting student.js

--- js/student.js
+++ js/student.js
@@ -2,26 +2,31 @@ $(document).ready( () => {
     const currentUser = SDK.Storage.load("currentUser");
     const userId = SDK.Storage.load("user_id");
 
+    // Function to select all sandwiches from database
     SDK.Items.getSandwiches( (error, items) => {
         if(error) {
             alert("Error fetching sandwiches.");
         }
         else {
+            // For each item in database, append the HTML-template for items
             items.forEach((item) => {
+
+                // HTML-template for items
                 const itemHtml =
                     `<div class='item'>
-                  <div class='itemImg'>
-                     <img src="${item.itemImg}">
-                  </div>
-                  <h3>${item.itemName}</h3>
-                  <p class="itemDescription">${item.itemDescription}</p>
-                  <p class="itemDescription">${item.itemPrice} DKK</p>
-                  <input class="button orderButton" data-item-id="${item.itemId}" type="submit" value="Add to cart">
-               </div>`;
+                        <div class='itemImg'>
+                            <img src="${item.itemImg}">
+                        </div>
+                        <h3>${item.itemName}</h3>
+                        <p class="itemDescription">${item.itemDescription}</p>
+                        <p class="itemDescription">${item.itemPrice} DKK</p>
+                        <input class="button orderButton" data-item-id="${item.itemId}" type="submit" value="Add to cart">
+                    </div>`;
 
                 $("#sandwiches").append(itemHtml);
             });
 
+            // When orderButton is clicked, add chosen item to basket
             $(".orderButton").click(function() {
                 const itemId = $(this).data("item-id");
                 const item = items.find((item) => item.itemId == itemId);
@@ -36,7 +41,10 @@ $(document).ready( () => {
             alert("Error fetching pastries.");
         }
         else {
+            // For each item in database, append the html-template for items
             items.forEach((item) => {
+
+                // HTML-template for items
                 const itemHtml =
                     `<div class='item'>
                   <div class='itemImg'>
@@ -51,6 +59,7 @@ $(document).ready( () => {
                 $("#pastries").append(itemHtml);
             });
 
+            // When orderButton is clicked, add chosen item to basket
             $(".orderButton").click(function() {
                 const itemId = $(this).data("item-id");
                 const item = items.find((item) => item.itemId == itemId);
@@ -65,7 +74,10 @@ $(document).ready( () => {
             alert("Error fetching smoothies.");
         }
         else {
+            // For each item in database, append the html-template for items
             items.forEach((item) => {
+
+                // HTML-template for items
                 const itemHtml =
                     `<div class='item'>
                   <div class='itemImg'>
@@ -80,6 +92,7 @@ $(document).ready( () => {
                 $("#smoothies").append(itemHtml);
             });
 
+            // When orderButton is clicked, add chosen item to basket
             $(".orderButton").click(function() {
                 const itemId = $(this).data("item-id");
                 const item = items.find((item) => item.itemId == itemId);
@@ -89,6 +102,7 @@ $(document).ready( () => {
         }
     });
 
+    // Function to create basket structure and prepare it for possible items to be added to basket
     createBasket = () => {
         const basket = SDK.Storage.load("basket") || [];
         const $basketContent = $("#basketContent");
@@ -98,23 +112,26 @@ $(document).ready( () => {
         $basketContent.empty();
         $totalSum.empty();
 
+        // For each entry, append the html-template for basket
         basket.forEach(entry => {
             let subtotal = entry.item.itemPrice * entry.count;
             total += subtotal;
+
             $basketContent.append(`
-            <div id="basketRow" class="basketRow">
-               <div class="itemRow">
-                  <p class="itemCount">${entry.count} stk</p>
-                  <p class="itemName">${entry.item.itemName}</p>
-                  <p class="itemPrice">${subtotal} DKK</p>
-               </div>
-               <div class="clearItem" data-item-id="${entry.item.itemId}">
-                  <img src="img/remove.svg">
-               </div>
-            </div>
-         `);
+                <div id="basketRow" class="basketRow">
+                   <div class="itemRow">
+                      <p class="itemCount">${entry.count} stk</p>
+                      <p class="itemName">${entry.item.itemName}</p>
+                      <p class="itemPrice">${subtotal} DKK</p>
+                   </div>
+                   <div class="clearItem" data-item-id="${entry.item.itemId}">
+                      <img src="img/remove.svg">
+                   </div>
+                </div>
+            `);
         });
 
+        // When clearItem is clicked, remove chosen item from basket and load the new basket
         $(".clearItem").click(function() {
             const itemId = $(this).data("item-id");
             SDK.Items.removeFromBasket(itemId);
@@ -126,11 +143,13 @@ $(document).ready( () => {
 
     createBasket();
 
+    // When clearBasketButton is clicked, remove all items in the basket and load the new basket
     $("#clearBasketButton").click(() => {
         SDK.Storage.remove("basket");
         createBasket();
     });
 
+    // When checkoutButton is clicked, add all items to selectedItems array and create an order with selected items
     $("#checkoutButton").click(() => {
         const basket = SDK.Storage.load("basket");
         const selectedItems = [];
@@ -150,9 +169,11 @@ $(document).ready( () => {
         });
     });
 
+    // Function to find all orders for the user that is logged in
     SDK.Orders.getOrdersById( (error, orders) => {
         if (error) throw error;
 
+        // For each order in database, append the html-template for orders
         orders.forEach((order) => {
             let status = "Not ready";
 
@@ -161,7 +182,6 @@ $(document).ready( () => {
             }
 
             let $items = "";
-            let $quantity = 0;
             let $total = 0;
 
             for (let i = 0; i < order.items.length; i++) {
@@ -169,6 +189,7 @@ $(document).ready( () => {
                 $total += order.items[i].itemPrice;
             }
 
+            // HTML-template for orders
             orderHtml =
                 `<div class="orderRow">
                <div class="orderValue">
@@ -196,6 +217,7 @@ $(document).ready( () => {
         })
     });
 
+    // When logOutButton is clicked, log out user
     $("#logOutButton").on('click', () => {
         SDK.logOut( () => {
             window.location.href = "index.html";
@@ -203,6 +225,7 @@ $(document).ready( () => {
     });
 });
 
+// Function to add functionality to the separate tabs
 function openTab(tab, tabNumber) {
     var i, tabcontent, tabs;
 
